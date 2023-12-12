85b71206735da43fbb3b637660519c2df8adb9e6
박용진 <insight.emoket@gmail.com>
2023-12-12 11:12:47 +0900

    LogoutIconButton on FrameTop added

---
+++ src/components/LogoutIconButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useUserAuth } from '../composables/useUserAuth';
+import { ionLogOutOutline } from '@quasar/extras/ionicons-v6';
+
+const { signOutUser } = useUserAuth();
+</script>
+
+<template>
+  <q-btn
+    stretch
+    flat
+    color="white"
+    :icon="ionLogOutOutline"
+    @click="signOutUser"
+  />
+</template>
+
+<style lang="scss" scoped></style>
