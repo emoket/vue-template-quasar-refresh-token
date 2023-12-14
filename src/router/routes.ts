372542c7363c06e6668adb2610b1505ff049f973
박용진 <insight.emoket@gmail.com>
2023-12-14 11:31:06 +0900

    dhx routes added

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,17 @@
 import { RouteRecordRaw } from 'vue-router';
 import authRouter from '../auth/router';
 import privateRouter from '../private-module/router';
+import dhxRouter from '../dhx-module/router';
 // import { requiereAuthGuard } from './GuardsAuth';
 
 export const routes: RouteRecordRaw[] = [
   {
     path: '/auth',
     ...authRouter,
+  },
+  {
+    path: '/dhx',
+    ...dhxRouter,
   },
   {
     path: '/',
