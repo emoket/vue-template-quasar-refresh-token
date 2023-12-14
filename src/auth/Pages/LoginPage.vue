7705a60684625441e39f2f6254f87e3481308810
박용진 <insight.emoket@gmail.com>
2023-12-14 11:30:00 +0900

    LoginPage flex-center

--- src/auth/Pages/LoginPage.vue
+++ src/auth/Pages/LoginPage.vue
@@ -29,82 +29,74 @@ const onReset = () => {
 </script>
 
 <template>
-  <q-layout>
-    <q-page-container>
-      <q-page class="flex bg-image flex-center">
-        <q-card style="width: 350px">
-          <q-card-section>
-            <q-avatar
-              size="100px"
+  <div class="flex flex-center">
+    <q-card bordered flat style="width: 350px">
+      <q-card-section>
+        <q-avatar
+          size="100px"
+          color="primary"
+          text-color="white"
+          class="absolute-center shadow-10"
+        >
+          L
+        </q-avatar>
+      </q-card-section>
+      <q-card-section>
+        <div class="text-center q-pt-lg">
+          <div class="col text-h6 ellipsis">Log in</div>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <q-form class="q-gutter-md" @submit="onSubmit" @reset="onReset">
+          <q-input
+            autofocus
+            filled
+            v-model="credentials.username"
+            ref="username"
+            label="사번 *"
+            hint="8 또는 9사번 8자리"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 7) || '필수 항목입니다.']"
+          >
+            <template v-slot:prepend>
+              <q-icon :name="ionPersonOutline" />
+            </template>
+          </q-input>
+
+          <q-input
+            filled
+            v-model="credentials.password"
+            ref="password"
+            label="비밀번호 *"
+            hint="LDAP Password"
+            lazy-rules
+            :rules="[(val) => (val && val.length > 0) || '필수 항목입니다.']"
+          >
+            <template v-slot:prepend>
+              <q-icon :name="ionLockClosedOutline" />
+            </template>
+          </q-input>
+
+          <q-card-actions>
+            <q-btn
+              label="로그인"
+              type="submit"
               color="primary"
-              text-color="white"
-              class="absolute-center shadow-10"
-            >
-              L
-            </q-avatar>
-          </q-card-section>
-          <q-card-section>
-            <div class="text-center q-pt-lg">
-              <div class="col text-h6 ellipsis">Log in</div>
-            </div>
-          </q-card-section>
-          <q-card-section>
-            <q-form class="q-gutter-md" @submit="onSubmit" @reset="onReset">
-              <q-input
-                autofocus
-                filled
-                v-model="credentials.username"
-                ref="username"
-                label="사번 *"
-                hint="8 또는 9사번 8자리"
-                lazy-rules
-                :rules="[
-                  (val) => (val && val.length > 7) || '필수 항목입니다.',
-                ]"
-              >
-                <template v-slot:prepend>
-                  <q-icon :name="ionPersonOutline" />
-                </template>
-              </q-input>
-
-              <q-input
-                filled
-                v-model="credentials.password"
-                ref="password"
-                label="비밀번호 *"
-                hint="LDAP Password"
-                lazy-rules
-                :rules="[
-                  (val) => (val && val.length > 0) || '필수 항목입니다.',
-                ]"
-              >
-                <template v-slot:prepend>
-                  <q-icon :name="ionLockClosedOutline" />
-                </template>
-              </q-input>
-
-              <q-card-actions>
-                <q-btn
-                  label="로그인"
-                  type="submit"
-                  color="primary"
-                  class="q-py-sm q-px-md"
-                />
-                <q-space />
-                <q-btn
-                  label="초기화"
-                  type="reset"
-                  color="negative"
-                  flat
-                  class="q-ml-sm"
-                />
-              </q-card-actions>
-            </q-form>
-          </q-card-section>
-        </q-card>
-      </q-page>
-    </q-page-container>
-  </q-layout>
+              class="q-py-sm q-px-md"
+            />
+            <q-space />
+            <q-btn
+              label="초기화"
+              type="reset"
+              color="negative"
+              flat
+              class="q-ml-sm"
+            />
+          </q-card-actions>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <style scoped>
