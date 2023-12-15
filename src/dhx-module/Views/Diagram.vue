ea777d999c1f2917a73bdcb20bbbca7c636d97ae
박용진 <insight.emoket@gmail.com>
2023-12-15 18:19:28 +0900

    Diagram added

---
+++ src/dhx-module/Views/Diagram.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page style="width: 100%; height: 100vh">
+    <div class="dhx_sample-container">
+      <div class="dhx_sample-widget" ref="editor"></div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import fromCDN from 'from-cdn';
+// import data
+import { autoPlaceData } from './diagram-data';
+
+export default {
+  name: 'DefaultEditorAutoplacement',
+  data: () => ({
+    editor: null,
+  }),
+  mounted() {
+    fromCDN([
+      'https://cdn.dhtmlx.com/diagram/pro/edge/diagramWithEditor.js',
+      'https://cdn.dhtmlx.com/diagram/pro/edge/diagramWithEditor.css',
+    ]).then(() => {
+      // eslint-disable-next-line no-undef
+      this.editor = new dhx.DiagramEditor(this.$refs.editor, {
+        type: 'default', // ['org', 'minimap']
+        autoplacement: {
+          mode: 'direct',
+        },
+      });
+      this.editor.parse(autoPlaceData);
+    });
+  },
+};
+</script>
