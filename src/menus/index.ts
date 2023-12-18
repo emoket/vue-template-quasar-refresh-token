73ec861187a6f2c66760ad9af66c93623e631aa4
박용진 <insight.emoket@gmail.com>
2023-12-18 14:20:36 +0900

    Pivot added

--- src/menus/index.ts
+++ src/menus/index.ts
@@ -11,6 +11,7 @@ import {
   symRoundedChecklist,
   symRoundedFamilyHistory,
   symRoundedHardDrive,
+  symRoundedPivotTableChart,
 } from '@quasar/extras/material-symbols-rounded';
 
 const privateMenus = [
@@ -32,6 +33,18 @@ const privateMenus = [
   },
 ];
 
+// https://dhtmlx.com/docs/products/
+// 1. Gantt (o)
+// 2. Scheduler (o)
+// 3. Suite - (esp. Grid) (o)
+// 4. Diagram (o)
+// 5. Spreadsheet (o)
+// 6. Kanban (x)
+// 7. To Do List (x)
+// 8. Event Calendar (x)
+// 9. Rich Text Editor (o)
+// 10. Vault (o)
+// 11. Pivot (o)
 const dhxMenus = [
   {
     title: '@dhx/trial-gantt',
@@ -77,6 +90,11 @@ const dhxMenus = [
     title: 'cdn-vault',
     icon: symRoundedHardDrive,
     to: '/dhx/vault',
+  },
+  {
+    title: 'cdn-pivot',
+    icon: symRoundedPivotTableChart,
+    to: '/dhx/pivot',
   },
 ];
 
