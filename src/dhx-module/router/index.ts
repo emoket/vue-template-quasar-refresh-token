2b8d9dd663881dd6fd232159d46b4a3019a32c52
박용진 <insight.emoket@gmail.com>
2024-01-04 17:38:36 +0900

    @ alias 적용

--- src/dhx-module/router/index.ts
+++ src/dhx-module/router/index.ts
@@ -1,4 +1,4 @@
-import DhxLayout from '../layout/DhxLayout.vue';
+import DhxLayout from '@/dhx-module/layout/DhxLayout.vue';
 
 export default {
   component: DhxLayout,
@@ -8,67 +8,67 @@ export default {
       path: '/dhx/gantt',
       name: 'dhx-gantt',
       meta: { requiresAuth: true },
-      component: () => import('../Views/Gantt.vue'),
+      component: () => import('@/dhx-module/components/Gantt.vue'),
     },
     {
       path: '/dhx/event-calendar',
       name: 'dhx-event-calendar',
       meta: { requiresAuth: true },
-      component: () => import('../Views/EventCalendar.vue'),
+      component: () => import('@/dhx-module/components/EventCalendar.vue'),
     },
     {
       path: '/dhx/scheduler',
       name: 'dhx-scheduler',
       meta: { requiresAuth: true },
-      component: () => import('../Views/Scheduler.vue'),
+      component: () => import('@/dhx-module/components/Scheduler.vue'),
     },
     {
       path: '/dhx/richtexteditor',
       name: 'dhx-richtexteditor',
       meta: { requiresAuth: true },
-      component: () => import('../Views/RichTextEditor.vue'),
+      component: () => import('@/dhx-module/components/RichTextEditor.vue'),
     },
     {
       path: '/dhx/suite',
       name: 'dhx-suite',
       meta: { requiresAuth: true },
-      component: () => import('../Views/Suite.vue'),
+      component: () => import('@/dhx-module/components/Suite.vue'),
     },
     {
       path: '/dhx/spreadsheet',
       name: 'dhx-spreadsheet',
       meta: { requiresAuth: true },
-      component: () => import('../Views/SpreadSheet.vue'),
+      component: () => import('@/dhx-module/components/SpreadSheet.vue'),
     },
     {
       path: '/dhx/todolist',
       name: 'dhx-todolist',
       meta: { requiresAuth: true },
-      component: () => import('../Views/ToDoList.vue'),
+      component: () => import('@/dhx-module/components/ToDoList.vue'),
     },
     {
       path: '/dhx/diagram',
       name: 'dhx-diagram',
       meta: { requiresAuth: true },
-      component: () => import('../Views/Diagram.vue'),
+      component: () => import('@/dhx-module/components/Diagram.vue'),
     },
     {
       path: '/dhx/vault',
       name: 'dhx-vault',
       meta: { requiresAuth: true },
-      component: () => import('../Views/VaultCdnParent.vue'),
+      component: () => import('@/dhx-module/components/VaultCdnParent.vue'),
     },
     {
       path: '/dhx/pivot',
       name: 'dhx-pivot',
       meta: { requiresAuth: true },
-      component: () => import('../Views/PivotEvents.vue'),
+      component: () => import('@/dhx-module/components/PivotEvents.vue'),
     },
     {
       path: '/dhx/kanban',
       name: 'dhx-kanban',
       meta: { requiresAuth: true },
-      component: () => import('../Views/Kanban.vue'),
+      component: () => import('@/dhx-module/components/Kanban.vue'),
     },
   ],
 };
